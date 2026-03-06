0db2cec973612d489d45d34dcd2c29ff552d4b77
Anand <anandsep7@gmail.com>
2026-03-06 20:20:00 +0530

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Production Node.js API running 🚀"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
